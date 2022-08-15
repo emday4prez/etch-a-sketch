337d105cf37824a9f607d1926a13e5df3e336f7c
Emerson <emerson.day@gmail.com>
2022-08-15 15:48:21 -0400

    added color changer

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-const container = document.querySelector('#container')
 
+
+const container = document.querySelector('#container')
+const root = document.querySelector(':root')
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows)
   container.style.setProperty('--grid-cols', cols)
@@ -15,17 +17,32 @@ makeRows(32, 32)
 
 const gridItem = document.querySelectorAll('.grid-item')
 
+
+
+
 let mouseDown = false
 document.body.onmousedown = () => (mouseDown = true)
 document.body.onmouseup = () => (mouseDown = false)
 
 gridItem.forEach((item) => {
-  // item.addEventListener('click', function() {
-  //   item.classList.add('colored')
-  // })
+  item.addEventListener('click', function() {
+    item.classList.add('colored')
+  })
   item.addEventListener('mouseover', (e) => {
     if (mouseDown) {
       item.classList.add('colored')
     }
   })
+})
+
+document.querySelector('.reset').addEventListener('click', () => {
+  gridItem.forEach((item) => {
+    item.classList.remove('colored')
+  }
+  )
+}) 
+const colorPicker = document.querySelector('#colorPicker')
+colorPicker.addEventListener('change', (e) => {
+  root.style.setProperty('--primary-color', e.target.value)
+  console.log(e.target.value)
 })
